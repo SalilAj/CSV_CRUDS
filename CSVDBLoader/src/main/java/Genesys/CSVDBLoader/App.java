86cc4e115b8c6198fc50c9c4ea01e11a41279e3f
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2018-11-26 00:22:27 +0000

    Able to read the CSV file and insert it into MongoDB

--- CSVDBLoader/src/main/java/Genesys/CSVDBLoader/App.java
+++ CSVDBLoader/src/main/java/Genesys/CSVDBLoader/App.java
@@ -2,16 +2,73 @@ package Genesys.CSVDBLoader;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.opencsv.CSVReader;
 
 public class App {
+	public static boolean dumpCSVData(File file, String DBurl) {
+
+		try {
+
+			MongoClient mongoClient = new MongoClient(new MongoClientURI(DBurl));
+			DB database = mongoClient.getDB("genesys");
+			DBCollection collection = database.getCollection("people");
+
+			FileReader filereader = new FileReader(file);
+			CSVReader csvReader = new CSVReader(filereader);
+
+			String[] nextRecord;
+			if (csvReader.readNext() != null) {
+				while ((nextRecord = csvReader.readNext()) != null) {
+					DBObject person = new BasicDBObject();
+					int columnNumber = 1;
+					for (String cell : nextRecord) {
+						if (columnNumber == 1) {
+							person.put("firstName", cell);
+						} else if (columnNumber == 2) {
+							person.put("lastName", cell);
+						} else if (columnNumber == 3) {
+							person.put("age", cell);
+						}
+						columnNumber++;
+					}
+					collection.insert(person);
+				}
+				System.out.println("Done!!!");
+			} else {
+				System.out.println("File was empty!!!");
+			}
+			csvReader.close();
+
+		} catch (UnknownHostException e) {
+			System.out.println("Invalid MongoDB URI. Please input a correct URI. Exiting program");
+			return false;
+		} catch (FileNotFoundException e) {
+			System.out.println("File doesnot exist. Please input a correct File. Program Exiting... \n");
+			return false;
+		} catch (IOException e) {
+			System.out.println("Error Reading CSV File. Program Exiting... \n");
+			return false;
+		}
+		return true;
+	}
 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String DBurl;
 		File file;
+		boolean result;
 
 		try {
 			System.out.println("Enter File Path:");
@@ -30,6 +87,12 @@ public class App {
 			return;
 		}
 
-		//callmongo insert
+		result = dumpCSVData(file, DBurl);
+
+		if (result) {
+			System.out.println("Entry to database was successful\n");
+		} else {
+			System.out.println("Failed to add entry to database\n");
+		}
 	}
 }
